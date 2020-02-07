75cc2efe57c80d2eb9a1c79bc1212f3114853668
Zak Ali <zak.ali@thetin.net>
2020-02-07 18:01:10 +0000

    Update

--- app/components/MusicHome.vue
+++ app/components/MusicHome.vue
@@ -1,22 +1,18 @@
 <template>
     <Page actionBarHidden="true">
-        <DockLayout stretchLastChild="false">
-            <GridLayout height="300" dock="top">
-                <Carousel col="0" row="0" verticalAlignment="top" background="red" height="100%" width="100%" pageChanged="myChangeEvent" pageTapped="mySelectedEvent" indicatorColor="red" finite="true" bounce="true" showIndicator="true" android:indicatorAnimation="swap" color="white">
-                    <CarouselItem id="slide1" backgroundColor="#b3cde0" verticalAlignment="middle">
-                        <Label text="Slide 1" backgroundColor="#50000000" horizontalAlignment="center"/>
-                    </CarouselItem>
-                    <CarouselItem id="slide2" backgroundColor="#6497b1" verticalAlignment="middle">
-                        <Label text="Slide 2" backgroundColor="#50000000" horizontalAlignment="center"/>
-                    </CarouselItem>
-                    <CarouselItem id="slide3" backgroundColor="#005b96" verticalAlignment="middle">
-                        <Label text="Slide 3" backgroundColor="#50000000" horizontalAlignment="center"/>
-                    </CarouselItem>
-                    <CarouselItem id="slide4" backgroundColor="#03396c" verticalAlignment="middle">
-                        <Label text="Slide 4" backgroundColor="#50000000" horizontalAlignment="center"/>
-                    </CarouselItem>
-                </Carousel>
-            </GridLayout>
+        <DockLayout stretchLastChild="true">
+            <AbsoluteLayout dock="top">
+                <GridLayout height="300" width="100%">
+                    <Carousel col="0" row="0" verticalAlignment="top" height="100%" width="100%" pageChanged="myChangeEvent" pageTapped="mySelectedEvent" indicatorColor="white" finite="true" bounce="true" showIndicator="true" android:indicatorAnimation="swap" color="white">
+                        <CarouselItem id="slide1" backgroundColor="#b3cde0" verticalAlignment="middle">
+                        </CarouselItem>
+                        <CarouselItem id="slide2" backgroundColor="#6497b1" verticalAlignment="middle">
+                        </CarouselItem>
+                        <CarouselItem id="slide3" backgroundColor="#005b96" verticalAlignment="middle">
+                        </CarouselItem>
+                    </Carousel>
+                </GridLayout>
+            </AbsoluteLayout>
             <Label text="Content"/>
         </DockLayout>
     </Page>
